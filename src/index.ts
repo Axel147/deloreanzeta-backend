a97f63c7db0550d4c52c72b768ab3cdcc689055b
Axel147 <lopez.b.axel@gmail.com>
2024-08-06 16:12:41 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(helmet());
-// app.use(express.json({ limit: '500mb'}));
+// app.use(express.json({ limit: '500mb'})); 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json({ type: 'application/json', limit: '500mb' }));
